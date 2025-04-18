e12cfd8d3c450654b2d80a5ca1cacf0b360fe44d
nishchyapratapsingh <np1416@srmist.edu.in>
2025-04-18 16:47:08 +0530

    alert and prompt

--- JS/basics/Guess Number/script.js
+++ JS/basics/Guess Number/script.js
@@ -5,7 +5,26 @@ alert("Guess a number between 0 and 20");
 
 while (num !== guess) {
     guess = parseInt(prompt("Enter a number: "));
-    if (num!==guess) alert("Guess again!");
+
+    if (guess === num) {
+        alert("Correct!");
+    }
+
+    else if (guess <= 12 && guess >= 6) {
+        alert("You are close!");
+    }
+
+    else if (guess > 12) {
+        alert("Too high");
+    }
+
+    else if (guess < 6) {
+        alert("Too low!");
+    }
+
+    else {
+        alert("Enter Valid number!");
+    }
 }
 
-alert("Correct!");
+alert("Thanks for playing");
