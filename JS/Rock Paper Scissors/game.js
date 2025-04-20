2d604ca655af5f066459c8af81f3416088c94527
nishchyapratapsingh <np1416@srmist.edu.in>
2025-04-20 23:15:36 +0530

    Disable buttons after game ends

--- JS/Rock Paper Scissors/game.js
+++ JS/Rock Paper Scissors/game.js
@@ -4,7 +4,10 @@ let compScore = 0;
 let userTurn;
 let compTurn;
 
-let turns = ["temp", "rock", "paper", "scissors"];
+let turns = ["temp", "Rock", "Paper", "Scissor"];
+
+let userTurnStatus = document.querySelector(".userTurnText");
+let compTurnStatus = document.querySelector(".compTurnText");
 
 let playButtons = document.querySelectorAll(".playBtn");
 
@@ -36,22 +39,33 @@ playButtons.forEach((btn) => {
       (userTurn == 3 && compTurn == 3)
     ) {
       statusText.innerText = `${turns[userTurn]} and ${turns[userTurn]} is a tie`;
-      compScore++;
     } else if (
       (userTurn == 1 && compTurn == 3) ||
       (userTurn == 2 && compTurn == 1) ||
       (userTurn == 3 && compTurn == 2)
     ) {
-        userScore++;
-        statusText.innerText = `${turns[userTurn]} beats ${turns[compTurn]}`;
+      userScore++;
+      statusText.innerText = `${turns[userTurn]} beats ${turns[compTurn]}`;
+    } else {
+      compScore++;
+      statusText.innerText = `${turns[compTurn]} beats your ${turns[userTurn]}`;
     }
-    else {
-        compScore++;
-        statusText.innerText = `${turns[compTurn]} beats your ${turns[userTurn]}`;
-    }
+
+    userTurnStatus.innerText = `\t${turns[userTurn]}`;
+    compTurnStatus.innerText = `\t${turns[compTurn]}`;
 
     userScoreElem.innerText = userScore;
     compScoreElem.innerText = compScore;
 
+    if (userScore >= 5 || compScore >= 5) {
+      userScore > compScore
+        ? (statusText.innerText = `Congratulations! You Won`)
+        : (statusText.innerText = `Comp wins! Try again`);
+
+        playButtons.forEach((btn)=> {
+            btn.style.pointerEvents = "none";
+            btn.style.opacity = "0.5";
+        })
+    }
   });
 });
