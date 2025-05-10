da47ca6485c9d182625dbea77eb01fd5dca5f152
nishchyapratapsingh <np1416@srmist.edu.in>
2025-05-10 18:34:52 +0530

    API fetch successful

--- JS/Currency Converter/app.js
+++ JS/Currency Converter/app.js
@@ -1,7 +1,9 @@
 const BASE_URL =
-  "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+  "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies";
 
 const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+let msg = document.querySelector(".msg");
 
 for (let select of dropdowns) {
   for (currCode in countryList) {
@@ -28,3 +30,23 @@ const updateFlag = (element) => {
   let image = element.parentElement.querySelector("img");
   image.src = newSrc;
 };
+
+btn.addEventListener("click", async (evt) => {
+  evt.preventDefault(); //prevents default behaviour of button (reload)
+  let amountElem = document.querySelector(".amountBox input");
+  let amount = amountElem.value;
+  if (amount === "" || amount < 1) {
+    amount = 1;
+    amountElem.value = "1";
+  }
+  const fromCountry = document.querySelector(".from select").value;
+  const toCountry = document.querySelector(".to select").value;
+  const URL = `${BASE_URL}/${fromCountry.toLowerCase()}.json`; //api calls should be lower case
+  let response = await fetch(URL);
+  let data = await response.json();
+  console.log(data);
+  let rate = data[fromCountry.toLowerCase()][toCountry.toLowerCase()];
+  let resultAmount = amount * rate;
+
+  msg.innerText = `${amount} ${fromCountry} = ${resultAmount} ${toCountry}`;
+});
