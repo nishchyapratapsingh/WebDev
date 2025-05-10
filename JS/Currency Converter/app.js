51bc4f14d2cb46e8ef78c5c36ff219bc8a043f01
nishchyapratapsingh <np1416@srmist.edu.in>
2025-05-10 18:05:29 +0530

    Back to work

---
+++ JS/Currency Converter/app.js
@@ -0,0 +1,30 @@
+const BASE_URL =
+  "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+
+for (let select of dropdowns) {
+  for (currCode in countryList) {
+    let newOption = document.createElement("option");
+    newOption.innerText = currCode;
+    newOption.value = currCode;
+    if (select.name === "from" && currCode === "USD") {
+      newOption.selected = "selected";
+    } else if (select.name === "to" && currCode === "INR") {
+      newOption.selected = "selected";
+    }
+    select.append(newOption);
+
+    select.addEventListener("change", (evt) => {
+      updateFlag(evt.target); //gives as argument the element changed
+    });
+  }
+}
+
+const updateFlag = (element) => {
+  let currCode = element.value;
+  let countryCode = countryList[currCode];
+  let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+  let image = element.parentElement.querySelector("img");
+  image.src = newSrc;
+};
